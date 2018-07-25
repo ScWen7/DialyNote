ec295f142aa0dd040c6b8cbe5f3876e1393d3a6b
ScWen7 <xxh783965@gmail.com>
2018-07-25 14:57:46 +0800

    1、发布动态界面布局

--- app/src/main/java/scwen/com/dialynote/ui/publish/PublishTopicActivity.java
+++ app/src/main/java/scwen/com/dialynote/ui/publish/PublishTopicActivity.java
@@ -2,7 +2,6 @@ package scwen.com.dialynote.ui.publish;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -10,26 +9,20 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import scwen.com.dialynote.R;
 import scwen.com.dialynote.appbase.BaseMvpActivity;
 import scwen.com.dialynote.contract.PublishContract;
 import scwen.com.dialynote.dialog.LoadingDialog;
 import scwen.com.dialynote.presenter.PublishPresenter;
 
-public class PublishTopicActivity extends AppCompatActivity {
+public class PublishTopicActivity extends BaseMvpActivity<PublishPresenter> implements PublishContract.PublishView {
 
 
-
-
-//    BaseMvpActivity<PublishPresenter> implements PublishContract.PublishView
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    //
     private LoadingDialog loadingDialog;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(getLayoutId());
-    }
 
 
     public static void start(Context context) {
@@ -37,21 +30,23 @@ public class PublishTopicActivity extends AppCompatActivity {
         context.startActivity(intent);
     }
 
-//    @Override
-//    protected void initView() {
-//        loadingDialog = new LoadingDialog(this);
-//        loadingDialog.setCanceledOnTouchOutside(false);
-//
-//
-//
-//    }
+    @Override
+    protected void initView() {
+        loadingDialog = new LoadingDialog(this);
+        loadingDialog.setCanceledOnTouchOutside(false);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mToolbar.setTitle(R.string.shortcut_label_publish_topic);
+
+    }
 
 
     //如果有Menu,创建完后,系统会自动添加到ToolBar上
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.activity_publish_topic, menu);
+        getMenuInflater().inflate(R.menu.menu_publish_topic, menu);
         return true;
     }
 
@@ -69,32 +64,34 @@ public class PublishTopicActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-//    @Override
+    @Override
     protected int getLayoutId() {
         return R.layout.activity_publish_topic;
     }
 
-//    @Override
-//    protected void initData() {
-//
-//    }
-//
-//    @Override
-//    public PublishPresenter createPresenter() {
-//        return new PublishPresenter(this);
-//    }
-//
-//    @Override
-//    public void showLoading() {
-//        if (loadingDialog != null && !loadingDialog.isShowing()) {
-//            loadingDialog.show();
-//        }
-//    }
-//
-//    @Override
-//    public void dismissLoading() {
-//        if (loadingDialog != null && loadingDialog.isShowing()) {
-//            loadingDialog.dismiss();
-//        }
-//    }
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    public PublishPresenter createPresenter() {
+        return new PublishPresenter(this);
+    }
+
+    @Override
+    public void showLoading() {
+        if (loadingDialog != null && !loadingDialog.isShowing()) {
+            loadingDialog.show();
+        }
+    }
+
+    @Override
+    public void dismissLoading() {
+        if (loadingDialog != null && loadingDialog.isShowing()) {
+            loadingDialog.dismiss();
+        }
+    }
+
+
 }
